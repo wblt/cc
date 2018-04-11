fc1ec2992eb6101b03fb786e6c882ab938ead1b2
wyChris <281269704@qq.com>
2018-04-11 13:02:26 +0800

    123

---
+++ cc/Modules/LoginAndRegist/VC/LoginVC.h
@@ -0,0 +1,13 @@
+//
+//  LoginVC.h
+//  cc
+//
+//  Created by yanghuan on 2018/4/11.
+//  Copyright © 2018年 wyChirs. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface LoginVC : BaseViewController
+
+@end
---
+++ cc/Modules/LoginAndRegist/VC/RegistViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegistViewController.h
+//  cc
+//
+//  Created by yanghuan on 2018/4/11.
+//  Copyright © 2018年 wyChirs. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface RegistViewController : BaseViewController
+
+@end
