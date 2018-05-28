9894525929a62c2fec3010e46cba08ec265aced1
wyChris <281269704@qq.com>
2018-05-28 12:39:53 +0800

    123

--- cc/Modules/Release/VC/InvitionFriendVC.m
+++ cc/Modules/Release/VC/InvitionFriendVC.m
@@ -27,7 +27,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    
+	self.navigationItem.title = @"邀请好友";
     [self requestData];
     [self addTapView];
 }
